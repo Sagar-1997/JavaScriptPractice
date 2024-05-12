d6305e94ef86174c509787d5940e7071aed97589
Sagar Rathore <71638801+Sagar-1997@users.noreply.github.com>
2024-05-12 21:28:37 +0000

    variables

---
+++ basics/variables.js
@@ -0,0 +1,9 @@
+const customerId = 12345;
+let customerName = "Jhon";
+var state = "kanataka";
+city = "Banglore";
+//customerId=555; not allowd once value is assign in constant we cannot change
+//re-declaration of variables not allowed in let and const
+//don't use var because it does not provide block scope 
+//we can declare let can initialzed after but not possible in const;
+console.table([customerId,customerName,state,city,code]);
